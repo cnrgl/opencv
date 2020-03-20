04af3a39d66ca1c453299ee67da73ed6025ffca5
cnrgl <cnrgl22@gmail.com>
2020-03-21 00:24:51 +0300

    Add files via upload

---
+++ 0.introduction/L0.0#image_show/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+int main(int argc, char** argv)
+{
+	String imageName("image.jpg"); // by default
+	if (argc > 1)
+	{
+		imageName = argv[1];
+	}
+	Mat image;
+	image = imread(samples::findFile(imageName), IMREAD_COLOR); // Read the file
+	/*
+	IMREAD_UNCHANGED (<0) loads the image as is (including the alpha channel if present)
+IMREAD_GRAYSCALE ( 0) loads the image as an intensity one
+IMREAD_COLOR (>0) loads the image in the RGB format
+*/
+	if (image.empty())                      // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	namedWindow("Display window",WINDOW_AUTOSIZE|WINDOW_FREERATIO); // Create a window for display WINDOW_NORMAL OR (WINDOW_KEEPRATIO) or not (WINDOW_FREERATIO).
+	imshow("Display window", image);                // Show our image inside it.
+	waitKey(0); // Wait for a keystroke in the window
+	return 0;
+}
